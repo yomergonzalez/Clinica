69cd7364a194e8d3877225723e0b00217163a404
yomer <ygmessiah@gmail.com>
2017-03-07 16:03:55 -0400

    receta

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -11,11 +11,17 @@ class Users extends CI_Controller {
         $this->load->model('Users_model');
         $usuario = $this->Users_model->verificar_usuario($this->input->post('email'),$this->input->post('password'));
         if($usuario){
-          $this->session->set_userdata('user', $usuario)
+          $this->session->set_userdata('user', $usuario);
         }else{
           $this->session->set_flashdata('msg', 'Error de Credenciales');
           redirect('welcome');
         }
         $this->load->view('templates/dashboard/dashboard', $data);
     }
+
+    public function receta() {
+
+        $this->load->view('user/receta');
+    }
+
 }
--- application/views/login.php
+++ application/views/login.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Inicio de sesion</title>
-        <!-- Tell the browser to be responsive to screen width -->
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <!-- Bootstrap 3.3.6 -->
-        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-        <!-- Ionicons -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-        <!-- Theme style -->
-        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/AdminLTE.min.css">
-        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/login.css">
-
-        <!-- AdminLTE Skins. Choose a skin from the css/skins
-             folder instead of downloading all of them to reduce the load. -->
-        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/skins/_all-skins.min.css">
-
-    </head>
+<?php $this->load->view('common/header'); ?>
 
   <body class="hold-transition full">
     <div class="login-box">
@@ -61,9 +39,4 @@
     <!-- /.login-box-body -->
     </div>
 
-        <!-- jQuery 2.2.3 -->
-        <script src="<?php echo base_url(); ?>assets/plugins/jQuery/jquery-2.2.3.min.js"></script>
-        <!-- Bootstrap 3.3.7 -->
-        <script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
-        </body>
-        </html>
+  <?php $this->load->view('common/footer'); ?>
